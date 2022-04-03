ae6adecc113f41f520f47b8920d312e8220cfe01
dimamarjan <dima.marjan@gmail.com>
2022-04-03 16:50:06 +0300

    update

--- src/redux/slices/superheroes/superheroes-operations.js
+++ src/redux/slices/superheroes/superheroes-operations.js
@@ -1,61 +1,48 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
+const HEROE_URL = process.env.REACT_APP_SERVER_URL;
+const HEROE_URL_UPD = process.env.REACT_APP_SERVER_URL_UPD;
+const HEROE_URL_DATA_UPD = process.env.REACT_APP_SERVER_URL_DATA_UPD;
+
 const getSuperheroes = createAsyncThunk("superheroes/get", async () => {
-  const { data } = await axios.get(
-    "https://superheroe-db-server.herokuapp.com/api/heroes"
-  );
+  const { data } = await axios.get(HEROE_URL);
   return data;
 });
 
 const getSuperheroeById = createAsyncThunk("superheroe/get", async (id) => {
-  const { data } = await axios.get(
-    `https://superheroe-db-server.herokuapp.com/api/heroes/${id}`
-  );
+  const { data } = await axios.get(`${HEROE_URL}/${id}`);
   return data;
 });
 
-const addSuperheroes = createAsyncThunk("superheroe/post", async (req) => {
-  const { data } = await axios.post(
-    "https://superheroe-db-server.herokuapp.com/api/heroes",
-    req,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
-  );
+const addSuperheroes = createAsyncThunk("superheroe/create", async (req) => {
+  const { data } = await axios.post(HEROE_URL, req, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
   return data;
 });
 
-const updateSuperheroes = createAsyncThunk("superheroe/post", async (req) => {
-  const { data } = await axios.patch(
-    `https://superheroe-db-server.herokuapp.com/api/heroes/update`,
-    req,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
-  );
+const updateSuperheroes = createAsyncThunk("superheroe/update", async (req) => {
+  const { data } = await axios.patch(HEROE_URL_UPD, req, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
   return data;
 });
 
 const updateDataSuperheroes = createAsyncThunk(
-  "superheroe/post",
+  "superheroe/updateData",
   async (req) => {
-    const { data } = await axios.patch(
-      `https://superheroe-db-server.herokuapp.com/api/heroes/updateHeroe`,
-      req
-    );
+    const { data } = await axios.patch(HEROE_URL_DATA_UPD, req);
     return data;
   }
 );
 
 const delSuperheroe = createAsyncThunk("superheroes/delete", async (id) => {
-  await axios.delete(
-    `https://superheroe-db-server.herokuapp.com/api/heroes/${id}`
-  );
+  await axios.delete(`${HEROE_URL}/${id}`);
   return;
 });
 
